e54e32586c2ce5600c9c6c3fc4c66919d93b84a9
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-11-13 21:24:36 +0545

    pseudo random

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ class PasswordGenerator:
         raise NotImplementedError("Sub classes must implemented this method")
 
     def get_pseudo_random(self,min_val, max_val):
-        pass
+        """Generate a pseudo-random number using simple math operations."""
+        seed = (min_val * 31 + max_val * 17) % 97  # Simple seed calculation
+        return (seed * 37 + self.length * 23) % (max_val - min_val + 1) + min_val
     
 class SimplePassword(PasswordGenerator):
     pass
